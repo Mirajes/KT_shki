c7cb0a061f258bd3bb73a716a7e68998a4a13748
Mirajes <myrrrclone@gmail.com>
2026-02-04 13:33:55 +0300

    Complited KT_1

---
+++ KT_shki/Helper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace KT_shki
+{
+    static class Helper
+    {
+        static public void ActionReseter(ref string action)
+        {
+            Console.Write("\n\n >> ");
+            action = Console.ReadLine();
+        }
+
+        static public void MakeAnIndentation(string name)
+        {
+            Console.WriteLine($"\n\n --- {name} --- \n");
+        }
+
+        static public Random random = new Random();
+    }
+}
+
+
+
+// perevod Enum v Array => Enum.GetValues(typeof(Countries)).Cast<Countries>()
